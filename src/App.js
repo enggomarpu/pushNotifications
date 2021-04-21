f3c834d1d17115535e8bf91353fdc5d1d93e9b12
Umar <enggomarpu@gmail.com>
2021-04-21 21:52:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,9 +24,19 @@ function App() {
     // Handle registration errors
     console.error(err);
   });
-
  
 }
+// Handle push notifications (only when web page is open)
+Pushy.setNotificationListener(function (data) {
+  // Print notification payload data
+  console.log('Received notification: ' + JSON.stringify(data));
+  setCount( count + 1 )
+  // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
+  let message = data.message || 'Test notification';
+
+  // Display an alert with message sent from 
+  alert('Received notification: ' + message);
+});
 
  
   const hitUrlNotify = () => {
