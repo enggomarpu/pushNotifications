ce41b9dea3b75cad8a80e315b0131ece24f63d7e
Umar <enggomarpu@gmail.com>
2021-04-22 10:45:50 +0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,17 +5,7 @@ import Pushy from 'pushy-sdk-web';
 const Header = () => {
     const [count, setCount] = useState(0);
     // Handle push notifications (only when web page is open)
-    Pushy.setNotificationListener(function (data) {
-        // Print notification payload data
-        console.log('Received notification: ' + JSON.stringify(data));
-        setCount(count + 1)
-        // Attempt to extract the "message" property from the payload: {"message":"Hello World!"}
-        let message = data.message || 'Test notification';
-
-        // Display an alert with message sent from 
-        //alert('Received notification: ' + message);
-    });
-
+    
     return (
         <div>
             <h2>Notification{count}</h2>
